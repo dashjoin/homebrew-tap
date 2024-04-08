7ea6847dc08e21f891f500d2ff73ce0a75e3e238
uw4 <uli.walther@gmail.com>
2024-04-08 15:28:07 +0200

    Update to latest jsonata-java. #1

--- jsonata-cli/tap/bin/jsonata.bat
+++ jsonata-cli/tap/bin/jsonata.bat
@@ -72,7 +72,7 @@ if "%JAVACMD%"=="" set JAVACMD=java
 
 if "%REPO%"=="" set REPO=%BASEDIR%\lib
 
-set CLASSPATH="%BASEDIR%"\etc;"%REPO%"\jsonata-cli-0.4-jar-with-dependencies.jar
+set CLASSPATH="%BASEDIR%"\etc;"%REPO%"\jsonata-cli-0.5-jar-with-dependencies.jar
 
 set ENDORSED_DIR=
 if NOT "%ENDORSED_DIR%" == "" set CLASSPATH="%BASEDIR%"\%ENDORSED_DIR%\*;%CLASSPATH%
